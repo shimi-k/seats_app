6570e7f74ac2715a1ddfdd9b1e3ad1ac6980aa74
K.Shimizu <okayamanomomo.t@gmail.com>
2023-10-15 13:44:48 +0900

    BottomにTabBarを追加

--- lib/pages/thirdPage.dart
+++ lib/pages/thirdPage.dart
@@ -5,13 +5,49 @@ class ThirdPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('School'),
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-      ),
-      body: Container(
-        color: Colors.green,
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('School'),
+          leading: Image.asset('images/owl.png'),
+          bottom: const TabBar(tabs: [
+            Tab(
+              text: 'people',
+              icon: Icon(Icons.people),
+            ),
+            Tab(
+              text: 'settings',
+              icon: Icon(Icons.settings),
+            )
+          ]),
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          actions: [
+            IconButton(
+              onPressed: () => print('Map pressed'),
+              icon: Icon(Icons.map),
+              highlightColor: Colors.blue,
+              splashColor: Colors.purple,
+            ),
+            TextButton(
+              style: ButtonStyle(
+                foregroundColor: MaterialStateProperty.all<Color>(Colors.blue),
+              ),
+              onPressed: () => print('Cart pressed'),
+              child: Text('Cart'),
+            )
+          ],
+        ),
+        body: TabBarView(
+          children: [
+            Container(
+              color: Colors.green,
+            ),
+            Container(
+              color: Colors.purple,
+            ),
+          ],
+        ),
       ),
     );
   }
